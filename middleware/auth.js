e29a22faabe32bf335dbdd1be5801dd18d921c01
linh2020 <cdev.linh@gmail.com>
2024-05-23 10:23:29 -0700

    Auth Middleware Complete

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,25 @@
+const jwt = require("jsonwebtoken");
+const CustomAPIError = require("../errors/custom-error");
+
 const authenticationMiddleware = async (req, res, next) => {
-  console.log(req.headers.authorization);
-  next();
+  // console.log(req.headers);
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer")) {
+    throw new CustomAPIError("No token provided", 401); // 401 - Unauthorized Error
+  }
+
+  const token = authHeader.split(" ")[1];
+  // console.log(token);
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    // console.log(decoded); // { id: 23, username: 'aaaa', iat: 1716454914, exp: 1716455214 }
+
+    const { id, username } = decoded;
+    req.user = { id, username };
+    next();
+  } catch (error) {
+    throw new CustomAPIError("Not authorized to access this route", 401);
+  }
 };
 
 module.exports = authenticationMiddleware;
