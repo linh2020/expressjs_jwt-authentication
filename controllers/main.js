d64d12252f4f7bfd417c39e6c2ca29c2bf52d6c1
linh2020 <cdev.linh@gmail.com>
2024-05-23 01:58:51 -0700

    Check Auth Header

--- controllers/main.js
+++ controllers/main.js
@@ -22,7 +22,15 @@ const login = async (req, res) => {
 };
 
 const dashboard = async (req, res) => {
-  console.log(req.headers);
+  // console.log(req.headers);
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer")) {
+    throw new CustomAPIError("No token provided", 401); // 401 - Unauthorized Error
+  }
+
+  const token = authHeader.split(" ")[1];
+  console.log(token);
+  
   const luckyNumber = Math.floor(Math.random() * 100);
   res.status(200).json({
     msg: `Hello, Users`,
